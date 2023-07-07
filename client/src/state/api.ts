673ecdf1edfa2d65bfc38bfa8585878d3e033652
Alemoh Rapheal <raphealenike@gmail.com>
2023-07-07 02:30:08 -0700

    Finance statistics dashboard

---
+++ client/src/state/api.ts
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  GetKpisResponse,
+  GetProductsResponse,
+  GetTransactionsResponse,
+} from "./types";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/" }),
+  reducerPath: "api",
+  tagTypes: ["Kpis", "Products", "Transactions"],
+  endpoints: (build) => ({
+    getKpis: build.query<Array<GetKpisResponse>, void>({
+      query: () => "kpi/kpis/",
+      providesTags: ["Kpis"],
+    }),
+    getProducts: build.query<Array<GetProductsResponse>, void>({
+      query: () => "product/products/",
+      providesTags: ["Products"],
+    }),
+    getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+      query: () => "transaction/transactions/",
+      providesTags: ["Transactions"],
+    }),
+    getRaphs: build.query({
+      query: () => `kpi/kpis/`,
+    }),
+  }),
+});
+
+export const { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery, useGetRaphsQuery } = api;
